b7fd73c3a1fcd6af0632d36cbffc38b16e9fc21d
AfrahSaleem786 <afrahsaleem91@gmail.com>
2026-03-05 13:30:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+﻿let count = localStorage.getItem("tasbihCount");
+
+if (count === null) {
+    count = 0;
+}
+
+document.getElementById("counter").innerText = count;
+
+document.getElementById("countBtn").onclick = function () {
+
+    count++;
+
+    document.getElementById("counter").innerText = count;
+
+    localStorage.setItem("tasbihCount", count);
+
+};
+
+document.getElementById("resetBtn").onclick = function () {
+
+    count = 0;
+
+    document.getElementById("counter").innerText = count;
+
+    localStorage.setItem("tasbihCount", count);
+
+};
+
+document.getElementById("modeBtn").onclick = function () {
+
+    document.body.classList.toggle("dark");
+
+};
